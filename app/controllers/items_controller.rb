b50ed72e0ee142c1a5e5eb33e1c0b8ab9666e4ba
角田光博 <>
2019-10-11 17:19:12 +0900

    CRUD機能実装完了(見た目整理未実装)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,7 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def new
@@ -11,6 +12,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def create
@@ -19,7 +21,17 @@ class ItemsController < ApplicationController
    redirect_to items_url,notice:"シェア『#{item.tittle}』しました"
   end
 
+  def update
+    item= Item.find(params[:id])
+    item.update!(item_params)
+    redirect_to item_url,notice:"シェア品「#{item.tittle}」を更新しました"
+  end
 
+  def destroy
+   item = Item.find(params[:id])
+   item.destroy
+   redirect_to items_url,notice:"シェア品『#{item.tittle}』削除しました"
+  end
 
   private
 
