00a9c78fe2afc32aa2c020602d1da70f3fd89586
角田光博 <>
2019-10-12 22:50:37 +0900

    ユーザー一覧追加 ユーザー詳細追加 (nameがdbに保存されない)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'users/index'
+  get 'users/show'
   devise_for :users
   root to:'items#index'
 
   resources :items
+  resources :users, :only => [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
