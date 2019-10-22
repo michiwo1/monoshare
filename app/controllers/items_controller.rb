b6f8ebcfed9b525db5e172f94b4419f5182e2b51
角田光博 <>
2019-10-22 23:33:29 +0900

    申請をキャンセルしても通知テーブルにレコードが残っているバグを解消

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -61,10 +61,12 @@ class ItemsController < ApplicationController
   def cancel
     rental = Rental.find_by(item_id: params[:item_id])
     rental.destroy
-    redirect_to items_path
     item = Item.find(params[:item_id])
     item.state = nil
     item.save!
+    item_notification = Notification.find_by(item_id: params[:item_id])
+    item_notification.destroy
+    redirect_to items_path
   end
 
   def approve
