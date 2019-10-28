262b1c099b609586db446923be246a8121eb4ab2
角田光博 <>
2019-10-28 10:52:52 +0900

    レイアウト変更 通知機能完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -81,25 +81,25 @@ class ItemsController < ApplicationController
   end
 
   def reject
+    item_notification = Item.find(params[:item_id])
+    item_notification.notification_reject(current_user)
     rental = Rental.find_by(item_id: params[:item_id])
     rental.destroy
     item = Item.find(params[:item_id])
     item.state = nil
     item.save!
-    item_notification = Notification.find_by(item_id: params[:item_id])
-    item_notification.destroy
-    item_notification_cancel = Item.find(params[:item_id])
-    item_notification_cancel.notification_reject(current_user)
     redirect_to items_path
   end
 
   def complete
+    item_notification = Item.find(params[:item_id])
+    item_notification.notification_complete(current_user)
     rental = Rental.find_by(item_id: params[:item_id])
     rental.destroy
-    redirect_to items_path
     item = Item.find(params[:item_id])
     item.state = 3
     item.save!
+    redirect_to items_path
   end
 
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -46,17 +46,18 @@ class Item < ApplicationRecord
   end
 
   def notification_approve(current_user)
+    rental = rentals.last
     # すでに「いいね」されているか検索
-    temp = Notification.where(["visitor_id = ? and visited_id = ? and item_id = ? and action = ? ", current_user.id, user_id, id, 'approve'])
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and item_id = ? and action = ? ", current_user.id, rental.user_id, id, 'approve'])
     # いいねされていない場合のみ、通知レコードを作成
     if temp.blank?
       notification = current_user.active_notifications.new(
         item_id: id,
-        visited_id: user_id,
+        visited_id: rental.user_id,
         action: 'approve'
       )
     # 自分の投稿に対するいいねの場合は、通知済みとする
-      if notification.visitor_id != notification.visited_id
+      if notification.visitor_id == notification.visited_id
         notification.checked = true
       end
       notification.save if notification.valid?
@@ -64,14 +65,34 @@ class Item < ApplicationRecord
   end
 
   def notification_reject(current_user)
+    rental = rentals.last
     # すでに「いいね」されているか検索
-    temp = Notification.where(["visitor_id = ? and visited_id = ? and item_id = ? and action = ? ", current_user.id, user_id, id, 'reject'])
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and item_id = ? and action = ? ", current_user.id, rental.user_id, id, 'reject'])
     # いいねされていない場合のみ、通知レコードを作成
     if temp.blank?
       notification = current_user.active_notifications.new(
         item_id: id,
-        visited_id: user_id,
+        visited_id: rental.user_id,
         action: 'reject'
+      )
+    # 自分の投稿に対するいいねの場合は、通知済みとする
+      if notification.visitor_id == notification.visited_id
+        notification.checked = true
+      end
+      notification.save if notification.valid?
+    end
+  end
+
+  def notification_complete(current_user)
+    rental = rentals.last
+    # すでに「いいね」されているか検索
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and item_id = ? and action = ? ", current_user.id, rental.user_id, id, 'complete'])
+    # いいねされていない場合のみ、通知レコードを作成
+    if temp.blank?
+      notification = current_user.active_notifications.new(
+        item_id: id,
+        visited_id: rental.user_id,
+        action: 'complete'
       )
     # 自分の投稿に対するいいねの場合は、通知済みとする
       if notification.visitor_id == notification.visited_id
