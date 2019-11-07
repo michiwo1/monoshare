c4c92d6e83d7e2f0989c45b504f4ac1e7e10601b
角田光博 <>
2019-11-07 16:19:46 +0900

    コメント通知機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,9 @@ class CommentsController < ApplicationController
     item = Item.find(params[:item_id])
     @comment = item.comments.build(comment_params)
     @comment.user_id = current_user.id
+    @notification = @comment.item
     if @comment.save
+      @notification.notification_comment(current_user, @comment.id)
       flash[:success] = "コメントしました"
       redirect_back(fallback_location: root_path)
     else
